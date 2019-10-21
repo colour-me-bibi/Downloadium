2a5347a68bbb61ce38eab60a4c469b2690e3b8e1
colour-me-bibi <melancholicredux.co@gmail.com>
2019-10-20 23:00:20 -0500

    Implemented threaded downloads all url types

---
+++ dl-manager.py
@@ -0,0 +1,1 @@
+# TODO read downloaded songs and strip from master list
--- downloadium.py
+++ downloadium.py
@@ -3,26 +3,41 @@ import urllib.parse
 import json
 import re
 import threading
+import os
 import config
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 # Google Drive authentication
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+# gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 
 def gDriveDownload(url):
     fileID = re.search(r'//.*(?:folders|d)/([^/,\n]*)', url).group(1)
-    print(fileID)
     driveFile = drive.CreateFile({'id': fileID})
-    print('title: %s, mimeType: %s' % (driveFile['title'], driveFile['mimeType']))
-    # pass
+
+    if 'folder' in driveFile['mimeType']:
+        fileList = drive.ListFile({'q': "'{}' in parents and trashed=false".format(fileID)}).GetList()
+
+        nestedDir = f"{config.saveDir}/{driveFile['title']}"
+
+        if not os.path.exists(nestedDir):
+            os.makedirs(nestedDir)
+
+        for siblingFile in fileList:
+            siblingFile.GetContentFile(f"{nestedDir}/{siblingFile['title']}", mimetype=siblingFile['mimeType'])
+        with open('downloaded.txt', 'a') as writeFile:
+            writeFile.write(url + '\n')
+    else:
+        driveFile.GetContentFile(f"{config.saveDir}/{driveFile['title']}", mimetype=driveFile['mimeType'])
+        with open('downloaded.txt', 'a') as writeFile:
+            writeFile.write(url + '\n')
 
 def download(url):
-    pass
-    # wget.download(url, config.saveDir)
-    # print(f'\ndownloaded {url} to {config.saveDir}')
+    wget.download(url, config.saveDir)
+    with open('downloaded.txt', 'a') as writeFile:
+            writeFile.write(url + '\n')
 
 with open('songs-20191009.jsonl') as file:
     for line in file:
@@ -33,12 +48,9 @@ with open('songs-20191009.jsonl') as file:
 
         domain = re.search(r'.*?://(.*?)/', url).group(1)
 
-        # TODO thread downloads
-
         if 'drive.google' in domain:
-            gDriveDownload(url)
-            # gThread = threading.Thread(target=gDriveDownload, args=[url])
-            # gThread.start()
+            gThread = threading.Thread(target=gDriveDownload, args=[url])
+            gThread.start()
         else:
             dThread = threading.Thread(target=download(url), args=[url])
             dThread.start()
