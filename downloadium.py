ae5d046cec23a4d18b191657a2919f3e7df14478
colour-me-bibi <melancholicredux.co@gmail.com>
2019-10-21 16:58:39 -0700

    Fixed regex and mimeType bugs

--- downloadium.py
+++ downloadium.py
@@ -15,8 +15,13 @@ gauth = GoogleAuth()
 # gauth.LocalWebserverAuth() # Needed only for initial auth
 drive = GoogleDrive(gauth)
 
+def writeLine(url):
+    with open('downloaded.txt', 'a') as writeFile:
+            writeFile.write(url + '\n')
+
 def gDriveDownload(url):
-    fileID = re.search(r'//.*(?:folders|d)/([^/,\n]*)', url).group(1)
+    fileID = re.search(r'//.*(?:/folders|/d)/([^/,\n]*)', url).group(1)
+
     driveFile = drive.CreateFile({'id': fileID})
 
     if 'folder' in driveFile['mimeType']:
@@ -28,18 +33,17 @@ def gDriveDownload(url):
             os.makedirs(nestedDir)
 
         for siblingFile in fileList:
-            siblingFile.GetContentFile(f"{nestedDir}/{siblingFile['title']}", mimetype=siblingFile['mimeType'])
-        with open('downloaded.txt', 'a') as writeFile:
-            writeFile.write(url + '\n')
+            if 'folder' not in siblingFile['mimeType']:
+                siblingFile.GetContentFile(f"{nestedDir}/{siblingFile['title']}", mimetype=siblingFile['mimeType'])
+
+        writeLine(url)
     else:
         driveFile.GetContentFile(f"{config.saveDir}/{driveFile['title']}", mimetype=driveFile['mimeType'])
-        with open('downloaded.txt', 'a') as writeFile:
-            writeFile.write(url + '\n')
+        writeLine(url)
 
 def download(url):
     wget.download(url, config.saveDir)
-    with open('downloaded.txt', 'a') as writeFile:
-            writeFile.write(url + '\n')
+    writeLine(url)
 
 with open('songs-20191009.jsonl') as file:
     for line in file:
